798f4f413cb05681aaf88b45b692fed2c906ddf4
TEJASTATODE <tejastatode449@gmail.com>
2025-09-13 18:50:14 +0530

    done

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://saas-notes-tmgu.onrender.com/api",
+  baseURL: "https://saas-notes-k36a.vercel.app/api",
   withCredentials: true,
 });
 export const setAuthToken = (token) => {
